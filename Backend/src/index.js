1dcf7bc5534e90726cbc38348278b2b8defbbca4
nalves599 <nuno.alves02@gmail.com>
2020-03-29 20:59:31 +0100

    Configurando porta do Heroku

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -9,6 +9,6 @@ app.use(express.json());
 app.use(cors());
 app.use(routes);
 
-app.listen(8085, () => {
+app.listen(process.env.PORT || 8085, () => {
     console.log('Server Listening on port 8085');
 });
